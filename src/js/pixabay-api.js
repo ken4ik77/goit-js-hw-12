6cfb09bff788e589de050ad693cf90bd79c1959e
ken4ik77 <ken4ik77@gmail.com>
2025-08-20 23:52:05 +0300

    1 change

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,14 +2,18 @@ import axios from "axios";
 
 const API_KEY = '51729015-e356512e01098e6cfb5d1c735';
 const BASE_URL = 'https://pixabay.com/api/';
+const PER_PAGE = 15;
 
-export function getImagesByQuery(query) {
+export async function getImagesByQuery(query, page) {
     const params = {
         key: API_KEY,
         q: query,
         image_type: "photo",
         orientation: "horizontal",
         safesearch: true,
+        page,
+        per_page: PER_PAGE
     };
-    return axios.get(BASE_URL, { params }).then(response => response.data,);
+    const response = await axios.get(BASE_URL, { params });
+    return response.data;
 };
